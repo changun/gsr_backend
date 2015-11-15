4fe74956fc276bc8fd34cec338d517afcdca1b38
Andy Hsieh <changun.tw@gmail.com>
2015-11-14 19:07:09 -0500

    add json library

--- project.clj
+++ project.clj
@@ -7,4 +7,7 @@
                  [http-kit "2.1.18"]
                  [compojure "1.4.0"]
                  [ring "1.4.0"]
-                 [clj-time "0.11.0"]])
+                 [clj-time "0.11.0"]
+                 [cheshire "5.5.0"]
+                 [ring/ring-codec "1.0.0"]
+                 ])
