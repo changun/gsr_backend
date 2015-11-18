4504a9f88a5c11e3ba42a6516a6380a0fa042cd1
Andy Hsieh <changun.tw@gmail.com>
2015-11-17 21:45:35 -0500

    update demo page. import data from Daniel

--- src/gsr_backend/core.clj
+++ src/gsr_backend/core.clj
@@ -7,6 +7,7 @@
             [monger.core :as mg]
             [monger.collection :as mc]
             [monger.joda-time]
+            [monger.query :as query]
             )
   (:use
         [gsr-backend.gmail]
@@ -38,12 +39,42 @@
                     :device-name device-name}
                    ))
       )
-    (catch Exception e (println e)
-                       ))
-  (send! channel "")
+    (catch Exception e (println e)))
+  (send! channel ""))
+
+
+
+(defn peak-detection [time]
+  (try (let [mail-period (->> (mc/find-maps db "readings" {:type "gsr"
+                                                           :time {:$gt (t/minus time (t/seconds 5))
+                                                                  :$lt (t/plus time (t/seconds 4))}} [:reading])
+                              (map :reading))
+             baseline-period (->> (mc/find-maps db "readings" {:type "gsr"
+                                                               :time {:$gt (t/minus time (t/minutes 1))
+                                                                      :$lt (t/minus time (t/seconds 5))}} [:reading])
+                                  (map :reading))]
+         (if (and (seq mail-period) (seq baseline-period) (> (count baseline-period) 20))
+           (let [mean (/ (apply + baseline-period) (count baseline-period))
+                 sigma (->> (map #(- % mean) baseline-period)
+                            (map #(Math/pow % 2))
+                            (apply +)
+                            )
+                 sigma (-> sigma
+                           (/ (- (count baseline-period) 1))
+                           (Math/pow 0.5)
+                           )
+                 mail-mean (/ (apply + mail-period) (count mail-period))
+                 peak? (> (/ (-  mail-mean mean) sigma) 1)]
+             (println "Peak? " peak?)
+             peak?
+             )
+
+           )
+
+
+         )
+       (catch Exception e nil))
   )
-
-
 
 (defn sync-mail [device-name access-token]
   (loop [last nil]
@@ -52,14 +83,19 @@
       (println msgs)
       (try
         (doseq [msg msgs]
-          (let [msg (get-msg (:id msg) access-token)]
+          (let [msg (get-msg (:id msg) access-token)
+                time (c/from-long (Long/valueOf ^String (:internalDate msg)))]
             (println msg)
-            (mc/save db "mails"
-                     (assoc
-                       msg
-                       :device-name device-name
-                       :time (c/from-long (Long/valueOf ^String (:internalDate msg)))
-                       :_id (:id msg)))
+            (future
+              (Thread/sleep 2000)
+              (mc/save db "mails"
+                       (assoc
+                         msg
+                         :device-name device-name
+                         :time time
+                         :peak? (peak-detection time)
+                         :_id (:id msg)))
+              )
             )
 
           )
@@ -89,21 +125,32 @@
                   )
                 ))
 (defn query [request]
-  (let [{:keys [start]} (:params request)
-        start (or start (t/minus (t/now) (t/minutes 10)))
+  (let [{:keys [start end]} (:params request)
+        start (if start (c/from-long (Long/valueOf start))
+                        (t/minus (t/now) (t/minutes 10)))
+        end (if end (c/from-long (Long/valueOf end))
+                    (t/plus start (t/minutes 10)))
         ]
     {:body (generate-string
              (merge
-               {:mail (->> (mc/find-maps db "mails" {:time {:$gte start}})
-                            (map #(dissoc % :_id)))
+               {:mail (->>
+                        (query/with-collection db "mails"
+                                               (query/find {:time {:$gte start :$lte end}})
+                                               (query/sort (array-map :time 1))
+                                               )
+                            (map #(dissoc % :_id))
+                           )
 
                 }
-               (->> (mc/find-maps db "readings" {:time {:$gte start}})
+               (->>
+                 (query/with-collection db "readings"
+                                        (query/find {:time {:$gte start :$lte end}})
+                                        (query/sort (array-map :time 1))
+                                        )
                     (map #(dissoc % :_id))
                     (group-by :type)
                     ))
              )
-
      }
     )
   )
@@ -116,3 +163,33 @@
            )
 
 (run-server (site #'all-routes) {:port 8080})
+
+
+
+(comment
+  (doseq [line (drop 1 (clojure.string/split (slurp "email.csv") #"\n"))]
+    (let [[dt time value] (clojure.string/split line #",")
+          dt (c/from-string (str "2015-11-17T" time "-05:00"))
+          ]
+      (mc/insert db "readings"
+                 {:time dt
+                  :type "gsr"
+                  :reading (Double/valueOf value)
+                  :device-name "test"
+                  :fake? true}
+                 )
+
+      )
+    )
+  (mc/save db "mails"
+           {:type "mail"
+            :device-name "test"
+            :time (c/from-string "2015-11-17T12:35:21-05:00")
+            :peak? true
+            :payload {:headers [{:name "Subject" :value "Update"}
+                                {:name "To" :value "Adam@ee.technion.ac.il"}
+                                ]}
+            :snippet "Hi Professor Shwartz,\n\nI just wanted to update you: \n\n- I sent a revised version of my seminar paper last Sunday to Professor Zaks, and I'm just waiting to hear back from him. "
+            :_id "fake"
+            :fake? true}
+           ))
